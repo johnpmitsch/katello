3d8448204e9de6523cbce77e9f4a7e123aeb59f8
Jeremy Lenz <jlenz@redhat.com>
2019-09-27 12:23:53 -0400

    Populate index.js

--- webpack/scenes/Fruits/index.js
+++ webpack/scenes/Fruits/index.js
@@ -1,5 +1,19 @@
 import { withRouter } from 'react-router-dom';
-
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as fruitActions from './FruitActions';
 import FruitPage from './FruitPage';
 
-export default withRouter(FruitPage);
+const mapStateToProps = state => ({
+  loading: state.katello.fruits.loading,
+  results: state.katello.fruits.results,
+  errors: state.katello.fruits.errors,
+});
+
+const actions = {
+  ...fruitActions,
+};
+
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(FruitPage));
